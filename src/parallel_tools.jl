ca0fee6f883e7fee59902403168355c1fd9ea211
paulstey <paulstey@gmail.com>
2016-08-15 16:58:43 -0400

    pcount_values() is working; decent speed up

--- src/parallel_tools.jl
+++ src/parallel_tools.jl
@@ -1,25 +1,23 @@
-addprocs(4)
-
-
+using Base.Threads
 
 v = [[1, 2, 3], [1, 2, 3], [1, 2, 3],  [1, 2, 5], [1, 3, 4], [1, 4, 5], [2, 3, 4], [2, 3, 4], [2, 3, 5], [3, 4, 5]]
-v = [rand([1, 2, 3, 4, 5], 10) for x = 1:100_000];
-
-dv = distribute(v)
+v = [rand([1, 2, 3, 4, 5], 10) for x = 1:1_000_000];
 
 
-function combine_dicts(d1, d2)
-    all_keys = [collect(keys(d1)); collect(keys(d2))]
+
+function combine_dicts!(d, d2)
+    all_keys = [collect(keys(d)); collect(keys(d2))]
     uniq_keys = unique(all_keys)
-    out = Dict()
 
     for k in uniq_keys
-        out[k] = get(d1, k, 0) + get(d2, k, 0)
+        d[k] = get(d, k, 0) + get(d2, k, 0)
     end
-    out
 end
 
 
+# Given an integer and a number of chunks, this function
+# returns a vector of ranges that partition 1:n into
+# nchnks total chunks.
 function gen_ranges(n, nchnks)
     step_size = round(Int64, (ceil(n/nchnks)))
     first_idx = 1
@@ -35,7 +33,9 @@ function gen_ranges(n, nchnks)
 end
 
 
-
+# Given a vector of vectors, this function returns a
+# Dict with counts representing the frequency of each
+# individual value.
 function count_values(u)
     out = Dict()
     for i = 1:length(u)
@@ -46,23 +46,29 @@ function count_values(u)
     out
 end
 
-function pcount_values(da)
-    np = length(procs(da))
-    dict_vec = Vector{Dict}(np)
-    @async for j = procs(da)
-        dict_vec[j-1] = remotecall_fetch(count_values, j, da)
+
+function pcount_values(v)
+    nt = nthreads()
+    dict_vec = Vector{Dict}(nt)
+    range_vec = gen_ranges(length(v), nt)
+
+    @threads for j = 1:nt
+        dict_vec[j] = count_values(v[range_vec[j]])
     end
-    dict_vec
+    cnt_dict = Dict()
+
+    for j = 2:nt
+        combine_dicts!(dict_vec[1], dict_vec[j])
+    end
+    return dict_vec[1]
 end
 
 
 
 
 
-@time count_values(dv)
+@time count_values(v)
+@time pcount_values(v)
+
 d1 = count_values(v[1:50_000])
 d2 = count_values(v[50_001:100_000])
-
-
-
-darray_closeall()
