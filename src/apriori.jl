bca92b98a449238a37d3fb08f4cfb44f5c19110a
paulstey <paulstey@gmail.com>
2016-08-17 17:01:16 -0400

    threading still slow with par_aprior_gen3()

--- src/apriori.jl
+++ src/apriori.jl
@@ -60,7 +60,7 @@ end
 # v = [rand([1, 2, 3, 4, 5, 6, 7, 8], 5) for x = 1:10000];
 # @code_warntype apriori_gen(v)
 # @time apriori_gen(v);
-#
+
 
 
 
--- src/parallel_tools.jl
+++ src/parallel_tools.jl
@@ -1,7 +1,7 @@
 using Base.Threads
 
-v = [[1, 2, 3], [1, 2, 3], [1, 2, 3],  [1, 2, 5], [1, 3, 4], [1, 4, 5], [2, 3, 4], [2, 3, 4], [2, 3, 5], [3, 4, 5]]
-v = [rand([1, 2, 3, 4, 5], 10) for x = 1:1_000_000];
+# v = [[1, 2, 3], [1, 2, 3], [1, 2, 3],  [1, 2, 5], [1, 3, 4], [1, 4, 5], [2, 3, 4], [2, 3, 4], [2, 3, 5], [3, 4, 5]]
+# v = [rand([1, 2, 3, 4, 5], 10) for x = 1:1_000_000];
 
 
 
@@ -81,39 +81,42 @@ function pcount_values(v)
     return dict_vec[1]
 end
 
+# @time count_values(v)
+# @time pcount_values(v)
+#
+# d1 = count_values(v[1:50_000])
+# d2 = count_values(v[50_001:100_000])
+#
 
 
 
-
-@time count_values(v)
-@time pcount_values(v)
-
-d1 = count_values(v[1:50_000])
-d2 = count_values(v[50_001:100_000])
-
-
-
-using Base.Threads
-
-function test_thread_conditional(n)
+function f1(n)
     out = zeros(Int, n)
     @threads for i = 1:n
         for j = 1:n
-            if j < n/2
-                out[j] = j^2
-            else
-                break
-            end
+            out[j] = j*i
         end
     end
     return out
 end
 
-test_thread_conditional(100)
+function f2(n)
+    out = zeros(Int, n)
+    for i = 1:n
+        @threads for j = 1:n
+            out[j] = j*i
+        end
+    end
+    return out
+end
+
+@code_warntype f1(100)
+@code_warntype f2(100)
 
 
-function init_nested_vectors(xtype, num)
-    inner = Array{xtype, 1}(0)
+
+function init_nested_vectors(typ, num, num_inner_elem = 0)
+    inner = Array{typ, 1}(num_inner_elem)
     outer = Array{Array, 1}(0)
 
     for i = 1:num
@@ -124,11 +127,14 @@ end
 
 init_nested_vectors(Array, 4)
 
+
+# This version of the function seems to work, but because of
+# an issue with the type of `i` being inferred as `Box.Core`,
+# the performance is no better than the serial implentation. This
+# should be fixed once the Github issue #15276 is resolved.
+
 function par_apriori_gen{M}(x::Array{Array{M, 1}, 1}, num_threads)
     n = length(x)
-    # if n < 1
-    #     return Array{Array{M, 1}, 1}(0)
-    # end
     m = length(x[1]) - 1
     C_arr = init_nested_vectors(Array, num_threads)
 
@@ -163,12 +169,91 @@ end
 
 
 
-function h(n)
-    res = Array{Int, 1}()
+
+
+function par_apriori_gen2{M}(x::Array{Array{M, 1}, 1}, num_threads)
+    n = length(x)
+
+    m = length(x[1]) - 1
+    C = Array{Array{M, 1}, 1}(0)
+
     for i = 1:n
-        for j = (i+1):n
-            push!(res, i*j)
+        @threads for j = (i+1):n
+
+            sort!(x[i])
+            sort!(x[j])
+            keep_candidate = true
+
+            # length k candidate itemsets are created by merging pairs of
+            # length k - 1 itemsets if their first k - 2 elements identical
+            for l in 1:m
+
+                # see if all k - 1 elements are identical
+                if x[i][l] != x[j][l] || x[i][m+1] == x[j][m+1]
+                    keep_candidate = false
+                    break
+                end
+            end
+            if keep_candidate
+                c::Array{M, 1} = vcat(x[i], x[j][end])
+                push!(C, sort!(c))
+            end
         end
     end
-    res
+    return C              # vector of candidate itemsets: C_{k}
+end
+
+v = [rand([1, 2, 3, 4, 5, 6, 7, 8], 5) for x = 1:10_000];
+
+
+
+
+function _par_apriori_gen{M}(x::Array{Array{M, 1}, 1}, num_threads, i)
+    n = length(x)
+    m = length(x[1]) - 1
+    C_arr = init_nested_vectors(Array, num_threads)
+
+    # for i = 1:n
+        # i::Int64 = 1
+    # while true
+        @threads for t = 1:num_threads
+        range_vec = gen_ranges(i+1, n, num_threads)
+            for j in range_vec[t]
+                sort!(x[i])
+                sort!(x[j])
+                keep_candidate = true
+
+                # length k candidate itemsets are created by merging pairs of
+                # length k - 1 itemsets if their first k - 2 elements identical
+                for l in 1:m
+
+                    # see if all k - 1 elements are identical
+                    if x[i][l] != x[j][l] || x[i][m+1] == x[j][m+1]
+                        keep_candidate = false
+                        break
+                    end
+                end
+                if keep_candidate
+                    c::Array{M, 1} = vcat(x[i], x[j][end])
+                    push!(C_arr[t], sort!(c))
+                end
+            end
+        end
+    #     i += 1
+    #     if i > n
+    #         break
+    #     end
+    # end
+    C::Array{Array{M, 1}, 1} = reduce(vcat, C_arr)
+    return C              # vector of candidate itemsets: C_{k}
+end
+
+
+
+function par_apriori_gen3{M}(x::Array{Array{M, 1}, 1}, num_threads, n)
+    C_arr = Array{Array{Array{M, 1}, 1}}(n)
+    for i = 1:n
+        C_arr[i] = _par_apriori_gen(x, num_threads, i)
+    end
+    res = reduce(vcat, C_arr)
 end
