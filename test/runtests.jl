b9ee6b8b319f710dfd5371ca3b04a30cb236494f
paulstey <paulstey@gmail.com>
2016-10-31 08:14:07 -0400

    added some more asserts to check spade output

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,11 +4,11 @@ using StatsBase                 # just for sample()
 transactions = [sample(1:10, 5, replace = false) for x in 1:100_000]
 fk = frequent(transactions, 0.1)
 
-groceries = ["steak", "chicken", "bread", "eggs", "apples", "oranges", "milk", "juice", "beer"]
+groceries = ["milk", "bread", "eggs", "apples", "oranges", "beer"]
 transactions = [sample(groceries, 4, replace = false) for x in 1:1000]
-@time fk = frequent(transactions, 0.1);
+fk = frequent(transactions, 0.1)
 
-@time rules1 = apriori(transactions, 0.1, 0.4, false);        # false for only single-item consequents
+rules1 = apriori(transactions, 0.1, 0.4, false);        # false for only single-item consequents
 display(rules1)
 
 # reading data from .csv
@@ -16,6 +16,8 @@ adult_data = readcsv("../data/adult.csv", skipstart = 1)
 adult_trans = make_transactions(adult_data[1:1000, :])  # take only sub-set of data for convenience
 rules2 = apriori(adult_trans, 0.2, 0.8)
 display(rules2)
+
+
 
 # testing SPADE algorithm
 zaki_data = readcsv("../data/zaki_data.csv", skipstart = 1)
@@ -25,6 +27,10 @@ seqs = make_sequences(zaki_data, sid_col = 2, eid_col = 3, item_col = 1)
 @assert length(res[1]) == 8
 @assert length(res[2]) == 57
 @assert length(res[3]) == 191
+@assert length(res[4]) == 444
+@assert length(res[5]) == 743
+@assert length(res[6]) == 897
+@assert length(res[7]) == 784
 
 
 # testing SPADE algorithm with subset of Zaki data
@@ -52,18 +58,18 @@ seqs3 = make_sequences(zaki_data, sid_col = 2, eid_col = 3, item_col = 1)
 @time seqrules3 = sequential_rules(res3, 0.01, 5);
 length(seqrules3)
 
-# letters = ["A", "B", "C", "D", "E", "F", "G"]
-# nseq = 10
-# ntime = 10
-n = 100
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"]
+n = 1000
+nseq = 20
+ntime = 50
+zaki_rep_data = hcat(rand(letters, n), rand(collect(1:nseq), n), rand(collect(1:ntime), n))
 
-zaki_rep_data = hcat(rand(zaki_data[:, 1], n), rand(zaki_data[:, 2], n), rand(zaki_data[:, 3], n));
-seqs4 = make_sequences(zaki_rep_data, sid_col = 2, eid_col = 3, item_col = 1);
+seqs4 = make_sequences(zaki_rep_data, sid_col = 2, eid_col = 3, item_col = 1)
 @time res4 = spade(seqs4, 0.2, 5);
 
 
 
-function benchmark1(n, k)
+function benchmark1(n)
     nseq = 10
     ntime = 10
     maxdepth = 10
@@ -75,4 +81,4 @@ function benchmark1(n, k)
     return nothing
 end
 
-benchmark1(10, 5)
+benchmark1(100)
