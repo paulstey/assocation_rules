b2d1d2c9b072483f4824bf721282ae071617f3e2
paulstey <paulstey@gmail.com>
2016-11-04 14:50:34 -0400

    added dataset() function for loading example datasets

--- src/apriori_utils.jl
+++ src/apriori_utils.jl
@@ -185,3 +185,24 @@ function display(rs::Vector{Rule}, colwidth = 30)
 end
 
 # display(rules)
+
+
+"""
+    dataset(dataset_name::AbstractString)
+
+This is a convenience function whose only purpose is to load example datasets provided with this package.
+
+### Arguments
+* `dataset_name`: A string specifying the dataset to be loaded (e.g., "zaki_data", "adult", etc).
+
+The returned value will be a dataframe. 
+"""
+function dataset(dataset_name::AbstractString)
+    filename = joinpath(dirname(@__FILE__), "..", "data", "$dataset_name.csv")
+
+    if !isfile(filename)
+        error(@sprintf "Unable to locate file %s \n" filename)
+    else
+        return readtable(filename)
+    end
+end
