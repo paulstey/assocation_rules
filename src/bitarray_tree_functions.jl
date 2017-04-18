523e57b1f834ecb5e271e482dab2d261d1b8419d
paulstey <paulstey@gmail.com>
2017-04-18 11:31:59 -0400

    few updates, testing suggest frequent itemset generation works

--- src/bitarray_tree_functions.jl
+++ src/bitarray_tree_functions.jl
@@ -1,3 +1,5 @@
+using StatsBase
+
 
 type Node
     id::Int16
@@ -26,8 +28,11 @@ type Node
     end
 end
 
+# @code_warntype Node(Int16(1), Int16[1], trues(3))
 
 n1 = Node(Int16(1), Int16[1], trues(3))
+
+# @code_warntype Node(Int16(1), Int16[1, 2], trues(3), n1)
 n2 = Node(Int16(1), Int16[1, 2], trues(3), n1)
 n3 = Node(Int16(1), Int16[1, 3], trues(3), n1)
 n4 = Node(Int16(1), Int16[1, 4], trues(3), n1)
@@ -37,6 +42,7 @@ n7 = Node(Int16(1), Int16[2, 3], trues(3), n1)
 n8 = Node(Int16(1), Int16[2, 4], trues(3), n1)
 n9 = Node(Int16(1), Int16[2, 5], trues(3), n1)
 n10 = Node(Int16(1), Int16[2, 6], trues(3), n1)
+
 
 push!(n1.children, n2)
 push!(n1.children, n3)
@@ -79,37 +85,23 @@ function growtree!(nd::Node, minsupp, k, maxdepth)
     end
 end 
 
+# @code_warntype growtree!(n2, 1, 3, 3)
 growtree!(n2, 1, 3, 3)
 
 
 
 function get_unique_items{M}(T::Array{Array{M, 1}, 1})
-    dict = Dict{M, Int}()
+    dict = Dict{M, Bool}()
 
     for t in T
         for i in t
-            dict[i] = 1
+            dict[i] = true
         end
     end
-    return [x for x in keys(dict)]
+    return collect(keys(dict))
 end
 
-
-function occurrence(T::Array{Array{String, 1}, 1})
-    n = length(T)
-    uniq_items = get_unique_items(T)
-    sort!(uniq_items)
-    p = length(uniq_items)
-    res = BitArray(n, p)
-
-    for j = 1:p 
-        for i = 1:n
-            res[i, j] = uniq_items[j] ∈ T[i]
-        end 
-    end 
-    res 
-end
-
+@time get_unique_items(t);
 
 
 function occurrence(T::Array{Array{String, 1}, 1}, uniq_items::Array{String,1})
@@ -124,6 +116,9 @@ function occurrence(T::Array{Array{String, 1}, 1}, uniq_items::Array{String,1})
     res 
 end
 
+unq = get_unique_items(t)
+@code_warntype occurrence(t, unq)
+
 
 function buildtree(T::Array{Array{String,1},1}, minsupp, maxdepth)
     uniq_items = get_unique_items(T)
@@ -133,11 +128,14 @@ function buildtree(T::Array{Array{String,1},1}, minsupp, maxdepth)
     root = Node(Int16(1), Int16[-1], BitArray(0))
     n_items = length(uniq_items)
 
+    # Creat 1-item nodes (i.e., first children)
     for j = 1:n_items
         nd = Node(Int16(j), Int16[j], occ[:, j], root)
         push!(root.children, nd)
     end
     n_kids = length(root.children)
+
+    # Grow nodes in breadth-first manner
     for j = 1:n_kids
         growtree!(root.children[j], minsupp, 2, maxdepth)
     end
@@ -154,8 +152,8 @@ t = [["a", "b"],
      ["a", "b", "c"],
      ["c", "b", "e"]]
 
-xtree1 = buildtree(t, 1)
-
+@code_warntype buildtree(t, 1, 3)
+xtree1 = buildtree(t, 1, 3)
 
 
 
@@ -173,16 +171,58 @@ groceries = ["asparagus", "broccoli", "carrots", "cauliflower", "celery",
              "tilapia", "tuna", "flour", "sugar", "yeast", 
              "cookies", "crackers", "nuts", "oatmeal", "popcorn",
              "pretzels", "cosmetics", "floss", "mouthwash", "toothpaste",
-             "lime", "almonds", "cashews", "ketchup", "mustard"]
+             "lime", "almonds", "cashews", "ketchup", "mustard",
+             "barley, quinoa", "couscous", "rice cakes", "pita bread",
+             "agave", "soy milk", "almond milk", "apple butter", "maple syrup",
+             "coffee filters", "granola", "bleach", "toaster pasteries", "glass cleaner",
+             "paper towels", "paper plates", "napkins", "sour cream", "pepperonis",
+             "buns", "rolls", "half and half", "steak sauce", "relish", 
+             "gravy", "lip balm", "goat cheese", "sunblock", "hand soap",
+             "muffins", "floss", "spaghetti", "bbq sauce", "vinegar"]
 
-transactions = [sample(groceries, 12, replace = false) for x in 1:1_000_000];
+# transactions = [sample(groceries, 12, replace = false) for x in 1:10];
 
 
 n = 100_000
-t = [sample(groceries, 50, replace = false) for _ in 1:n];
+m = 10              # number of items in transactions
+t = [sample(groceries, m, replace = false) for _ in 1:n];
 
 # @code_warntype buildtree(t, 1)
-@time f = buildtree(t, round(Int, n*0.2));
+@time f = buildtree(t, round(Int, n*0.1), m);
+
+
+"""
+    randstring(n, len)
+
+This function generates `n` random strings of length `len`. The strings will 
+be concatenated characters uniformally drawn from all alphabetic characters 
+(uppers and lowercase), as well as the numbers 0 to 9.
+"""
+function randstring(n::Int, len::Int = 16)
+    vals = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+               "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    upper = map(uppercase, vals)
+    append!(vals, upper)
+    append!(vals, map(string, 0:9))
+    res = Array{String,1}(n)
+    for i = 1:n
+        res[i] = join(rand(vals, len))
+    end
+    res
+end
+
+
+
+itemlist = randstring(500);
+
+
+n = 1_000_000
+m = 10              # number of items in transactions
+t = [sample(itemlist, m, replace = false) for _ in 1:n];
+
+# @code_warntype buildtree(t, 1)
+@time f = buildtree(t, round(Int, n*0.1), m);
+
 
 
 
@@ -196,90 +236,10 @@ t = [sample(groceries, 50, replace = false) for _ in 1:n];
 
 
 @code_warntype get_unique_items(t)
-@code_warntype occurrence(t)
 
 occ1 = occurrence(t)
 
 
 
 
-
-function transactions_to_nodes(T::Array{Array{String,1},1})
-    n = length(T)
-    uniq_items = get_unique_items(T)
-    sort!(uniq_items)
-    p = length(uniq_items)
-    occur = falses(n, p)
-
-    for j = 1:p 
-        @simd for i = 1:n
-            @inbounds occur[i, j] = uniq_items[j] ∈ T[i]
-        end 
-    end 
-    nodes = Array{Node,1}(p)
-    for j = 1:p
-        @inbounds nodes[j] = Node([Int16(j)], occur[:, j])
-    end 
-    
-
-    return nodes
-end
-
-
-# Get size of largest transaction 
-function max_transaction(T::Array{Array{String,1},1})
-    res = 0
-    n = length(T)
-    for i = 1:n
-        len = length(T[i])
-        res = (len > res) ? len : res 
-    end
-    res 
-end
-
-
-
-function merge_nodes(node1, node2, k)
-    ids = Array{Int,1}(k)
-    ids[1:k-1] = deepcopy(node1.item_ids[1:(k-1)])
-    if k == 2
-        ids[k] = node2.item_ids[1]
-    elseif k > 2
-        ids[k] = node2.item_ids[k-1]
-    end
-    transacts = node1.transacts & node2.transacts
-    nd = Node(ids, transacts)
-    return nd 
-end
-
-
-function gen_children(parent::Node, k, minsupp = 1)
-
-    nodes 
-end
-
-
-
-
-function frequent(T::Array{Array{String,1},1}, minsupp = 0)
-    nodes = transactions_to_nodes(T)
-    max_items = max_transaction(T)
-
-    F = Array{Array{Node,1},1}(max_items)
-    F[1] = nodes
-    k = 2
-
-    while k <= max_items 
-        # println(k)
-        F[k] = gen_next_layer(F[k-1], minsupp)
-        k += 1
-    end
-    F
-end
-
-n = 100_000
-t = [sample(groceries, 20, replace = false) for _ in 1:n];
-
-@code_warntype frequent(t, 1)
-@time f = frequent(t, round(Int, n*0.2));
 
